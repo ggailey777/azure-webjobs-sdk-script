fabiocav/azure-functions-host

Loading registered custom extensions

src/WebJobs.Script/BindingExtensions/IExtensionsManager.cs
@@ -1,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Script.Models;
+
+namespace Microsoft.Azure.WebJobs.Script.BindingExtensions
+{
+    public interface IExtensionsManager
+    {
+        Task<IList<ExtensionPackageReference>> GetExtensions();
+
+        Task AddExtensions(params ExtensionPackageReference[] reference);
+
+        Task DeleteExtensions(params string[] extensionIds);
+    }
+}

ggailey777/azure-webjobs-sdk-script

src/WebJobs.Script/BindingExtensions/IExtensionsManager.cs
@@ -1,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Script.Models;
+
+namespace Microsoft.Azure.WebJobs.Script.BindingExtensions
+{
+    public interface IExtensionsManager
+    {
+        Task<IEnumerable<ExtensionPackageReference>> GetExtensions();
+
+        Task AddExtensions(params ExtensionPackageReference[] reference);
+
+        Task DeleteExtensions(params string[] extensionIds);
+    }
+}