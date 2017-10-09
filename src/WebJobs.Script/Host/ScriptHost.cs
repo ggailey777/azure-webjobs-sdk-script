Azure/azure-functions-host

Add worker file watch for default extension

src/WebJobs.Script/Host/ScriptHost.cs
@@ -483,6 +483,9 @@ namespace Microsoft.Azure.WebJobs.Script
                         HandleHostError(evt.Exception);
                     }));
 
+                _subscriptions.Add(EventManager.OfType<RestartHostEvent>()
+                    .Subscribe((msg) => ScheduleRestartAsync(false)));
+
                 if (ScriptConfig.FileWatchingEnabled)
                 {
                     _fileEventSource = new FileWatcherEventSource(EventManager, EventSources.ScriptFiles, ScriptConfig.RootScriptPath);

ggailey777/azure-webjobs-sdk-script

src/WebJobs.Script/Host/ScriptHost.cs
@@ -487,6 +487,9 @@ namespace Microsoft.Azure.WebJobs.Script
                         HandleHostError(evt.Exception);
                     }));
 
+                _subscriptions.Add(EventManager.OfType<HostRestartEvent>()
+                    .Subscribe((msg) => ScheduleRestartAsync(false)));
+
                 if (ScriptConfig.FileWatchingEnabled)
                 {
                     _fileEventSource = new FileWatcherEventSource(EventManager, EventSources.ScriptFiles, ScriptConfig.RootScriptPath);